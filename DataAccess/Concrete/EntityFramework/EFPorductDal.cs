c5e6561f02340963bf8ce8c2461187d698e1f24a
Mahmut Güneş <115086298+Mhmt2534@users.noreply.github.com>
2024-03-19 22:10:21 +0300

    19/03/24

--- DataAccess/Concrete/EntityFramework/EFPorductDal.cs
+++ DataAccess/Concrete/EntityFramework/EFPorductDal.cs
@@ -26,38 +26,5 @@ public class EFPorductDal : IProductDal
 
     public void Delete(Product entity)
     {
-        using (NorthwindContext context = new NorthwindContext())
-        {
-            var deletedEntity = context.Entry(entity); 
-            deletedEntity.State = EntityState.Deleted;
-            context.SaveChanges();
-        }
-    }
 
-    public void Update(Product entity)
-    {
-        using (NorthwindContext context = new NorthwindContext())
-        {
-            var updatedEntity = context.Entry(entity); //bağlamdaki verilen entity nesnesini all
-            updatedEntity.State = EntityState.Modified;//durumu EntityState. ile ayarlarız
-            context.SaveChanges();
-        }
-    }
-
-    public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
-    {
-        using (NorthwindContext context=new NorthwindContext())
-        {
-            return filter == null ? context.Set<Product>().ToList() 
-                : context.Set<Product>().Where(filter).ToList();
-        }
-    }
-
-    public Product Get(Expression<Func<Product, bool>> filter)
-    {
-        using (NorthwindContext context=new NorthwindContext())
-        {
-            return context.Set<Product>().SingleOrDefault(filter);
-        }
-    }
 }
